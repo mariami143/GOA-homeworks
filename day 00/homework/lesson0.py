93f91a9e53ea2cc8251a323810fa50a30ad2593f
mariami143 <mariamchigladze28@gmail.com>
2025-02-22 19:21:29 +0400

    commit

---
+++ day 00/homework/lesson0.py
@@ -0,0 +1,48 @@
+from turtle import *
+#bulding house
+
+color ("red")
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(70)
+left(90)
+#start of the door
+
+color('green')
+
+forward(120)
+right(90)
+
+forward(60)
+right(90)
+
+forward(120)
+left(90) #the end of the door
+
+color("red")
+
+forward(70)
+left(90)
+
+forward(200)
+left(50)
+
+#starting the roof
+
+color("blue")
+
+forward(130)
+left(80)
+forward(130)
+exitonclick()
---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,6 @@
+name = "mariam"
+surname = "chigladze"
+age = "12"
+hight = "1,67"
+
+print("i am " + name + " " + "my surname is " + surname + "." + "i am " + str(age) + "." + "my hight is " + str(hight)) 
