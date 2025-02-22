7499ff8af3377d9d81873c4514ce4be905a4ae0d
mariami143 <mariamchigladze28@gmail.com>
2025-02-22 20:52:43 +0400

    commit

---
+++ day 02/homework/lesson.py
@@ -0,0 +1,9 @@
+print("homework")
+
+name =  "mari"
+hobby = "listening to music"
+reason = "it make me happy"
+hobby2 = "drawing"
+reason2 = "it also makes me happy and it helps me relax"
+
+print("my name is " + name + "," + "my hobby is " + hobby + "," + "reason behind is that " + reason + "," + "my other hobby is " + hobby2 + " " + "becouse," + reason2 + "." )
