935f160ee484f7e1e8051d552bf65b1aacd22375
mariami143 <mariamchigladze28@gmail.com>
2025-03-07 19:58:47 +0400

    commit

---
+++ day 04/lesson4.py
@@ -0,0 +1,35 @@
+#1.
+x=143
+z=149
+y=148
+
+print(x, z, y)
+
+#2.
+num1=143
+num2=149
+
+print(num1 + num2)
+
+#3.
+str1="i love"
+str2="mom"
+
+result = str1 + " " + str2
+
+print(result)
+
+#4.
+value=145
+print("old value:", value)
+
+value=144
+print("new value:", value)
+
+#5.
+#python
+#number = 5
+#print("Result: " + numb)
+
+number = 5
+print("result:"+str(number))
