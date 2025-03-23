6d2457e18d57550c08199d34315b66b76379e452
mariami143 <mariamchigladze28@gmail.com>
2025-03-23 12:52:12 +0400

    GOA

---
+++ day 06/classwork/lesson.py
@@ -0,0 +1,16 @@
+#age = input("please input your age: ")
+#print (age)
+
+#print ("i will ask you one more thing")
+#name = input("input your name: ")
+#print (name)
+
+
+
+nam1 = input("input a namber: ")
+nam2 = input("input a second namber: ")
+print (int(nam1) + int(nam2))
+print (int(nam1) - int(nam2))
+print (int(nam1) * int(nam2))
+print (int(nam1) / int(nam2))
+
---
+++ day 06/homework/lesson06.py
@@ -0,0 +1,14 @@
+nam1 = input("input your favorite namber")
+print("my favorite number is" + nam1)
+
+
+
+nam = int(input('input a number: '))
+print (type(nam))
+
+nam = "15"
+print(int(nam))
+
+#data type გვაძლევს საშვაქლებას რომ გავიგოთ თუ როგორი ტიპის ცვლადია ჩვენი შექმნილი ცვლადი
+
+#input-ით მომხმარებელს ვაძლევთ უფლებას რომ შეიყვანოს ინფორმაცია კოდში
