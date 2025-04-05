6045983b8b56cdaf648a637e3ee76592832e520f
mariami143 <mariamchigladze28@gmail.com>
2025-04-05 23:21:10 +0400

    GOA

---
+++ day 08/classwork/lesson.py
@@ -0,0 +1,31 @@
+num = (input("please input a number"))
+num2 = (input("please input a second number"))
+print(num > num2)
+print(num < num2)
+print(num == num2)
+print(num <= num2)
+print(num >= num2)
+print(num != num2)
+
+#True
+print(9 > 4) 
+print(5 < 8)
+print(7 == 7)
+print(6 != 5)
+print(3 <= 9)
+print(8 >= 7)
+
+#False
+print(5 > 9)
+print(6 < 5)
+print(8 == 3)
+print(2 != 2)
+print(6 >= 8)
+print(7 <= 2)
+
+hight = float((input("please input your hight: ")))
+friendhight = float((input("please input your friends hight: ")))
+
+print("you are taller than your friend: " , hight > friendhight)
+print("your friend is taller than you: " , hight < friendhight)
+print("You and your friend are the same height: " , hight == friendhight)
